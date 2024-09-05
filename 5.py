fa8c92520077eeeffbf840c579af202d6ad52b54
Kay <sergkadet@yandex.ru>
2024-09-05 20:15:51 +0300

    *n

--- 5.py
+++ 5.py
@@ -1,4 +1,33 @@
+"""
 while True:
     x = input("Enter text: ")
     if x == "stop": break
     print(x)
+
+while True:
+    y = input("Enter text: ")
+    if y == 'stop':
+        break
+    elif y.isalpha():
+        print(y * 8)
+    else:
+        num = int(y)
+        if num < 20:
+            print('low')
+        else:
+            print(num ** 3)
+"""
+
+nu, pu = 1, 2
+nudle, wingde = nu, pu
+print(nudle, wingde)
+
+string = "SPAM"
+(a, b), c = string[:2], string[2:]
+print(a,b,c)
+
+L = [1, 2, 3, 4]
+while True:
+    font, L = L[0], L[1:]
+    print(font, L)
+        
