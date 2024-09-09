5d486917d4e4bba112b827a99da6699f3fe053ce
Kay <sergkadet@yandex.ru>
2024-09-09 20:26:44 +0300

    or/and | if/else | while(continue, pass, break)

--- 6.py
+++ 6.py
@@ -11,3 +11,17 @@ bec = {'spam': 1,
        'ter': 3}
 print(bec.get('spam', 'Bad choice'))
 print(bec.get('heri', 'Bad choice'))
+
+print(2 or 3, [] or 3, {} or [])
+print(2 and 3, [] and 3, {} and [])
+x = 1
+y = 0
+z = 3
+m = y if x else z
+print(m)
+a = ((x and y) or z)
+print(a)
+if x and y:
+    print(1)
+else:
+    print(2)
