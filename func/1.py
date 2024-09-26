c0c1e485b357ea76388e0a81cc61ae10b970c975
Kay <sergkadet@yandex.ru>
2024-09-26 19:10:19 +0300

    def/def

--- func/1.py
+++ func/1.py
@@ -8,3 +8,18 @@ def instens(seq1, seq2):
 x = 'spamM'
 p = 'sram'
 print(instens(x, p))
+
+m = 100
+def loc1():
+    m = 110
+    def loc2():
+        print(x)
+    loc2()
+loc1()
+
+def mark(n):
+    def act(x):
+        return x ** n
+    return act
+meb = mark(10)
+print(meb(2))
