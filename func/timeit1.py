4499e457f52e66d58c2722413743b807c7badce4
unknown <sergkadet@yandex.ru>
2024-11-05 21:26:45 +0300

    timeit\2.7\3.7\PyPy

---
+++ func/timeit1.py
@@ -0,0 +1,6 @@
+import sys, timeit
+
+print(sys.version)
+print(min(timeit.repeat(stmt="[x ** 2 for x in range(1000)]", number=1000, repeat=5)))
+
+print(timeit.repeat(stmt="list(map(lambda x: x ** 2, range(1000)))", number=1000, repeat=5))
