4c94e4bc976b34afcfd1b5da93c5f2c2f51165ea
unknown <sergkadet@yandex.ru>
2024-11-07 21:03:19 +0300

    /

--- func/timeit1.py
+++ func/timeit1.py
@@ -4,3 +4,4 @@ print(sys.version)
 print(min(timeit.repeat(stmt="[x ** 2 for x in range(1000)]", number=1000, repeat=5)))
 
 print(timeit.repeat(stmt="list(map(lambda x: x ** 2, range(1000)))", number=1000, repeat=5))
+
